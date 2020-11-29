582ff41a1193767c4f50cc03b9fefcbe7312023a
dgreen8443 <44809262+dgreen8443@users.noreply.github.com>
2020-11-29 15:19:56 +0000

    Added auth for Access.py, listing repos belonging to users in json form

--- Access.py
+++ Access.py
@@ -5,5 +5,16 @@ access_token = '1a23bd36a62a85df3fd3188c76c5591ab8ff7125'
 my_headers = {'Authorization' : '{access_token}'}
 
 
-response = requests.get('https://api.github.com/repos/dgreen8443/Connect4', headers = my_headers)
-print(response.json())
+response = requests.get('https://api.github.com/users/dgreen8443/repos')
+
+y = response.json()
+#x = json.loads(y)
+for i in y:
+	print(json.dumps(i, indent = 4))
+	print(i["full_name"])
+
+#print(json.dumps(x, indent = 4))
+
+response = requests.get('https://api.github.com/repos/dgreen8443/SWENG-Group-9/languages', headers = my_headers)
+y = response.json()
+print(json.dumps(y, indent = 4))
