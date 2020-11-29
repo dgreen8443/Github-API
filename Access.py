8cd42d8775f2cc1838c3e0f745ecba3b85e4bfe8
dgreen8443 <44809262+dgreen8443@users.noreply.github.com>
2020-11-29 18:26:23 +0000

    Read authkey and user from externfiles, cleaning up with comments

--- Access.py
+++ Access.py
@@ -1,53 +1,79 @@
 import requests, json
 import subprocess
 import sys
-access_token = 'd4671af90b713d32019f549dfcf447d1a80465f3'
-my_headers = {'dgreen8443' : '{access_token}'}
+
+with  open('./authkey.txt', 'r') as reader:
+	access_token = reader.read()
+with open('./user.txt', 'r') as reader:
+	username = reader.read()
+reader.close()
+my_headers = {'{username}' : '{access_token}'}
 user_list = []
 repo_list = []
+
+
 def get_users(user):
 	url = 'https://api.github.com/users/' + user + '/following'
-	response = requests.get(url,auth=('dgreen8443','d4671af90b713d32019f549dfcf447d1a80465f3'))#headers = my_headers)
-	print(response)
+	response = requests.get(url,auth=(username, access_token))#headers = my_headers)
+	
 	for i in response.json():
 		user_list.append(i["login"])
 	
-def find_repos(user):
+
+	## goes through a user's list of public repos and finds the language that has the most lines of code in it
+def find_repos(user): 
 	url = 'https://api.github.com/users/' + user + '/repos'
-	response = requests.get(url,auth=('dgreen8443','d4671af90b713d32019f549dfcf447d1a80465f3'))#headers = my_headers)
-	print(response)
+	response = requests.get(url,auth=(username, access_token))#headers = my_headers)
+	
+	user_lang = []
 	for i in response.json():
 		repo_list.append(i["full_name"])
 		res = get_language(i["full_name"])
-		for i in res:
-			print(i)
+		user_lang.append(res)
+	print(user_lang)
+	for i in user_lang:
+		if i == {'blank' : 0}:
+			user_lang.remove(i)
+	print(user_lang)
 
 
+
+## takes a user's dictionary and returns a dictionary of the largest language in the repo and the assosiated #lines of code
 def get_language(repo):
 	url = 'https://api.github.com/repos/' + repo + '/languages'
+	repo_lang = []
+	repo_dict = {}
+	response = requests.get(url,auth=(username, access_token ))#headers = my_headers)
+	y = response.json()
+	for i in y:
+		repo_lang.append(i)
+	i = 0
+	while i < len(repo_lang):
+		repo_dict[repo_lang[i]] = y[repo_lang[i]]
+		i = i+1
+	
+	max_lang = {"blank": 0}
 
-	response = requests.get(url,auth=('dgreen8443','d4671af90b713d32019f549dfcf447d1a80465f3'))#headers = my_headers)
-	#print(response.json())
-	return response
+	#only take the language with most lines of code
+	for i in repo_dict:
+		for j in max_lang:
+			if(max_lang[j] < repo_dict[i]):
+				max_lang.clear()
+				max_lang[i] = repo_dict[i]
+	#print(max_lang)
+	
+	return max_lang
 	
 
 get_users('dgreen8443')
 print(user_list)
 for i in user_list:
 	find_repos(i)
-print(repo_list)
-for i in repo_list:
-	get_language(i)
-#y = response.json()
+#print(repo_list)
 
-#x = json.loads(y)
-#for i in y:
-	#print(json.dumps(i, indent = 4))
-#	print(i["login"])
-	
 
+
+response = requests.get('https://api.github.com/repos/dgreen8443/SWENG-Group-9/languages',auth=(username, access_token))#headers = my_headers)
+y = response.json() 
 #print(json.dumps(y, indent = 4))
-
-response = requests.get('https://api.github.com/repos/dgreen8443/SWENG-Group-9/languages',auth=('dgreen8443','d4671af90b713d32019f549dfcf447d1a80465f3'))#headers = my_headers)
-y = response.json()
-print(json.dumps(y, indent = 4))
+#
