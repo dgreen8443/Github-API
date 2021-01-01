b8c12e89cf57e8ffdc764a8850c2ddd9d1ae1de3
dgreen8443 <44809262+dgreen8443@users.noreply.github.com>
2021-01-01 14:58:26 +0000

    Corrected path on my-docker.sh

--- my-docker.sh
+++ my-docker.sh
@@ -2,5 +2,5 @@ echo $1>target.txt;
 echo $2>user.txt;
 echo $3>auth.txt;
 docker build -t github-access . ;
-docker run -t -v "$(pwd)":/external github-access ;
+docker run -t -v "$(pwd)"/output:/external github-access ;
 #python3 Open_photos.py
